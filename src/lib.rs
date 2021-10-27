306ba77f4fcb1e3c8942ae73ae0bd2417a540a2c
Romain Ruetschi <romain@informal.systems>
2021-10-27 23:02:16 +0200

    Reset version to 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@
 //!
 //! [`eyre::EyreHandler`]: https://docs.rs/eyre/*/eyre/trait.EyreHandler.html
 //! [`eyre`]: https://docs.rs/eyre
-#![doc(html_root_url = "https://docs.rs/oneline-eyre/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/oneline-eyre/0.1.0")]
 #![warn(
     missing_debug_implementations,
     missing_docs,
